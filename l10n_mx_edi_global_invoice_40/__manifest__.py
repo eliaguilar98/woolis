480b30220fbe112ad505d2e232aee00ab9756584
Pvalero70 <98065340+Pvalero70@users.noreply.github.com>
2024-03-01 14:30:16 -0600

    Update __manifest__.py

--- l10n_mx_edi_global_invoice_40/__manifest__.py
+++ l10n_mx_edi_global_invoice_40/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Factura Global CFDI 4.0',
-    'version': '17.0.0.0.1',
+    'version': '17.0.0.0.2',
     'category': 'Tools',
     'author': 'INFLEXYON',
     'website': '',
